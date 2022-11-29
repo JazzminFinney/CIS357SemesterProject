9f3db0209c1a53872411611770c1241aae298c2f
JazzminMariah <finnejaz@mail.gvsu.edu>
2022-11-28 20:12:14 -0500

    Initial Commit

---
+++ MobileSemesterProject/MobileSemesterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileSemesterProjectApp.swift
+//  MobileSemesterProject
+//
+//  Created by Jazzmin Finney on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MobileSemesterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
